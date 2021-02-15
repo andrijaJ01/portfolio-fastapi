463e831ce4fdb5c9554858aec7fcfdfc5c40631d
Andrijaj01 <andrijajovanovic001@gmail.com>
2021-02-15 05:23:01 +0100

    removed few pages

--- app/main.py
+++ app/main.py
@@ -3,8 +3,8 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
-from .library.helpers import *
-from app.routers import twoforms, unsplash, accordion , portfolio, root
+from .library.helpers import openfile
+from app.routers import  unsplash , portfolio, root
 
 from fastapi_cache.types import CacheControl
 from fastapi_cache.middleware import CacheControlMiddleware
@@ -19,8 +19,6 @@ templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app.include_router(unsplash.router)
-app.include_router(twoforms.router)
-app.include_router(accordion.router)
 app.include_router(portfolio.router)
 app.include_router(root.router)
 
