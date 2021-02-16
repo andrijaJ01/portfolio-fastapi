2c5f7ca63e1cebdae0bfb7bb1d8b50a0837503e3
Andrijaj01 <andrijajovanovic001@gmail.com>
2021-02-16 16:05:58 +0100

    working contact form

--- app/routers/contact.py
+++ app/routers/contact.py
@@ -2,6 +2,30 @@ from fastapi import FastAPI, Request, Form, APIRouter
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 import os
+import smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+
+def send(name,mail,message):
+    msg=MIMEMultipart()
+    msg['Subject'] = f'{name} wants to say hi'
+    msg['From'] = 'loggerk420@gmail.com'
+    message=f'{name}<{mail}>\n\n{message}'
+    text = MIMEText(str(message))
+    msg.attach(text)
+    s = smtplib.SMTP('smtp.gmail.com', '587')
+    s.ehlo()
+    s.starttls()
+    s.ehlo()
+    s.login('loggerk420@gmail.com','mojasobasobica')
+    s.sendmail('loggerk420@gmail.com', 'andrijajovanovic001@gmail.com', msg.as_string())
+    s.quit()
+
+
+
+
+
 
 router = APIRouter()
 templates = Jinja2Templates(directory="templates/")
@@ -12,6 +36,7 @@ def form_get(request: Request):
     return templates.TemplateResponse('contact.html', context={'request': request})
 
 @router.post('/contact')
-async def contact(name: str = Form(...), email: str = Form(...), message:str = Form(...)):
+async def contact(request: Request,name: str = Form(...), email: str = Form(...), message:str = Form(...)):
     print(f'name:\t\t{name}\nemail:\t\t{email}\nmessage:\t\t{message}\n')
-    return {'name':name,'email':email,'message':message}
+    send(name,email, message)
+    return templates.TemplateResponse('contact.html', context={'request': request,'name':name,'email':email,'message':message})
